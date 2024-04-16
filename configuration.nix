3c97471181b19aee59a137b97b12c6b9f6bdaa7c
loki-odin-devtest <loki_odin_devtest@protonmail.com>
2024-04-16 14:56:43 -0500

    updating option name

--- configuration.nix
+++ configuration.nix
@@ -90,8 +90,8 @@
    security.sudo.wheelNeedsPassword = false;  
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "alternex";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "alternex";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
