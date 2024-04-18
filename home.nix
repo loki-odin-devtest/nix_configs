dbe2c9580e40574b0b0b4c685e47f130b08b3f77
loki-odin-devtest <loki_odin_devtest@protonmail.com>
2024-04-18 08:21:34 -0500

    add shell_aliases for git and clean up in home.nix

--- home.nix
+++ home.nix
@@ -4,27 +4,6 @@
   home.username = "alternex";
   home.homeDirectory = "/home/alternex";
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # set cursor size and dpi for 4k monitor
-  #xresources.properties = {
-  #  "Xcursor.size" = 16;
-  #  "Xft.dpi" = 172;
-  # };
-
   # Set dconf settings
   dconf.settings = {
     "org/gnome/desktop/wm/preferences" = {
@@ -45,8 +24,6 @@
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    
-
     neofetch
     nnn # terminal file manager
 
@@ -122,24 +99,19 @@
     # Bash History Control
     historyControl = [ "ignoredups" "ignorespace" ];   
 
-    # set some aliases, feel free to add more or remove some
     shellAliases = {
      s = "sudo poweroff";
      eza = "eza -blah";
      bat = "bat --paging=never";
      exa = "eza -blah";
-     # k = "kubectl";
+    
+     # Git Aliases
+     gs = "git status";
+     ga = "git add";
+     gc = "git commit -m";
     };
   };
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "23.05";
 
   # Let home Manager install and manage itself.
