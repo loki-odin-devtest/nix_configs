f6b22dc54061f6ad3dca14a2dd0a59d0f3e793c0
loki-odin-devtest <loki_odin_devtest@protonmail.com>
2024-07-05 16:19:50 -0500

    attempt to disable sound

--- configuration.nix
+++ configuration.nix
@@ -69,15 +69,15 @@
 ]);
 
  #  Enable sound with pipewire.
- # sound.enable = true;
- # hardware.pulseaudio.enable = false;
- # security.rtkit.enable = true;
- # services.pipewire = {
- #   enable = true;
- #   alsa.enable = true;
- #   alsa.support32Bit = true;
- #   pulse.enable = true;
- # };
+ sound.enable = false;
+ hardware.pulseaudio.enable = false;
+ security.rtkit.enable = false;
+ services.pipewire = {
+    enable = false;
+    alsa.enable = false;
+    alsa.support32Bit = false;
+    pulse.enable = false;
+  };
 
   users.users.alternex = {
     isNormalUser = true;
